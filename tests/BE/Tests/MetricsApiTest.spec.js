5d10b278f64c52e75df3ab5ddf6f88f54d1ffcce
Mikheil Soziashvili <mikheil.soziashvili@externals.adidas.com>
2023-11-20 22:12:18 +0400

    Added line breaks, added missing await and asserts

--- tests/BE/Tests/MetricsApiTest.spec.js
+++ tests/BE/Tests/MetricsApiTest.spec.js
@@ -8,66 +8,89 @@ test.describe('Tests for metrics-expose API @ONEPLFR-352', async () => {
     })
 
     test('Successful request', async ({request}) => {
-        const response = request.get('', {headerWithApiKey: testDataForMetricsExpose.headerWithApiKey, params: testDataForMetricsExpose.paramsBody})
+        const response = await request.get('', {
+            headers: testDataForMetricsExpose.headerWithApiKey,
+            params: testDataForMetricsExpose.paramsBody
+        })
         const responseBody = await response.json()      
           
         await expect(response).toBeOk()
-        await expect(responseBody).toBeEqual(testDataForMetricsExpose.positiveResponse)
+        await expect(responseBody).toEqual(testDataForMetricsExpose.positiveResponse)
     })
 
     test('Request without Api Key header', async ({request}) => {
-        const response = request.get('', {headerWithoutApiKey: testDataForMetricsExpose.headerWithoutApiKey, params: testDataForMetricsExpose.paramsBody})
+        const response = await request.get('', {
+            headers: testDataForMetricsExpose.headerWithoutApiKey,
+            params: testDataForMetricsExpose.paramsBody
+        })
         const responseBody = await response.json()
 
         // await expect(response).not.toBeOk()
-        // await expect(responseBody).toBeEqual(testDataForMetricsExpose.errorResponseNone)
+        // await expect(responseBody).toEqual(testDataForMetricsExpose.errorResponseNone)
     })
 
     test('Request with wrong Api Key header', async ({request}) => {
-        const response = request.get('', {headerWithWrongApiKey: testDataForMetricsExpose.headerWithWrongApiKey, params: testDataForMetricsExpose.paramsBody})
+        const response = await request.get('', {
+            headers: testDataForMetricsExpose.headerWithWrongApiKey,
+            params: testDataForMetricsExpose.paramsBody
+        })
         const responseBody = await response.json()
 
         // await expect(response).not.toBeOk()
-        // await expect(responseBody).toBeEqual(testDataForMetricsExpose.errorResponseWrong)
+        // await expect(responseBody).toEqual(testDataForMetricsExpose.errorResponseWrong)
     })
 
     test('Request without parameters', async ({request}) => {
-        const response = request.get('', {headerWithWrongApiKey: testDataForMetricsExpose.headerWithWrongApiKey})
+        const response = await request.get('', {
+            headers: testDataForMetricsExpose.headerWithWrongApiKey
+        })
         const responseBody = await response.json()
 
         // await expect(response).not.toBeOk()
-        // await expect(responseBody).toBeEqual()  //Will update after resolving current defect
+        // await expect(responseBody).toEqual()  //Will update after resolving current defect
     })
 
     test('Request with only date', async ({request}) => {
-        const response = request.get('', {headerWithApiKey: testDataForMetricsExpose.headerWithApiKey, params: testDataForMetricsExpose.paramsBody['date']})
+        const response = await request.get('', {
+            headerWithApiKey: testDataForMetricsExpose.headerWithApiKey,
+            params: testDataForMetricsExpose.paramsBody['date']
+        })
         const responseBody = await response.json()
 
         // await expect(response).not.toBeOk()
-        // await expect(responseBody).toBeEqual()  //Will update after resolving current defect
+        // await expect(responseBody).toEqual()  //Will update after resolving current defect
     })
 
     test('Request with only leanIxId', async ({request}) => {
-        const response = request.get('', {headerWithApiKey: testDataForMetricsExpose.headerWithApiKey, params: testDataForMetricsExpose.paramsBody['leanIxId']})
+        const response = await request.get('', {
+            headerWithApiKey: testDataForMetricsExpose.headerWithApiKey,
+            params: testDataForMetricsExpose.paramsBody['leanIxId']
+        })
         const responseBody = await response.json()
         
         // await expect(response).not.toBeOk()
-        // await expect(responseBody).toBeEqual()  //Will update after resolving current defect
+        // await expect(responseBody).toEqual()  //Will update after resolving current defect
     })
 
     test('Request with different Content-Type', async ({request}) => {
-        const response = request.get('', {headerWithDifferentContentType: testDataForMetricsExpose.headerWithDifferentContentType, params: testDataForMetricsExpose.paramsBody})
+        const response = await request.get('', {
+            headerWithDifferentContentType: testDataForMetricsExpose.headerWithDifferentContentType, 
+            params: testDataForMetricsExpose.paramsBody
+        })
         const responseBody = await response.json()
 
         // await expect(response).not.toBeOk()
-        // await expect(responseBody).toBeEqual()  //Will update after resolving current defect
+        // await expect(responseBody).toEqual()  //Will update after resolving current defect
     })
 
     test('Request with different values', async ({request}) => {
-        const response = request.get('', {headerWithApiKey: testDataForMetricsExpose.headerWithApiKey, params: testDataForMetricsExpose.paramsBodyDifferent})
+        const response = await request.get('', {
+            headerWithApiKey: testDataForMetricsExpose.headerWithApiKey, 
+            params: testDataForMetricsExpose.paramsBodyDifferent
+        })
         const responseBody = await response.json()
 
         // await expect(response).toBeOk()
-        // await expect(responseBody).toBeEqual()  //Will update after resolving current defect
+        // await expect(responseBody).toEqual()  //Will update after resolving current defect
     })
 })
