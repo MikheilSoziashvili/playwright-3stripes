8aa768cc7f824147cdb82d3e82271874003349b5
Mikheil Soziashvili <mikheil.soziashvili@externals.adidas.com>
2023-11-20 21:26:44 +0400

    Changed name of the test plan

--- tests/BE/Tests/MetricsApiTest.spec.js
+++ tests/BE/Tests/MetricsApiTest.spec.js
@@ -1,7 +1,7 @@
 const { test, expect, request } = require('@playwright/test');
 import * as testDataForMetricsExpose from '../../../test-data/test-data-for-metrics-expose';
 
-test.describe('Tests for metrics-expose API @ONEPLFR-322', async () => {
+test.describe('Tests for metrics-expose API @ONEPLFR-352', async () => {
     
     test.use({
         baseURL: testDataForMetricsExpose.apiUrl
