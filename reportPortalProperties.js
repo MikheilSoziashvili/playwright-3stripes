533add7329274dfcdecbbcf5acea906d6b9c8516
Mikheil Soziashvili <mikheil.soziashvili@externals.adidas.com>
2023-11-29 18:52:21 +0400

    Testing out presigned url request

--- reportPortalProperties.js
+++ reportPortalProperties.js
@@ -3,7 +3,7 @@ const RPconfig = {
   token: process.env.RP_TOKEN, //Do not modify - Env variable passed in jenkinsFile
   endpoint: "https://testreportingportal.tools.3stripes.net/api/v1", //Do not modify
   project: process.env.RP_PROJECT, //Do not modify - Env variable passed in jenkinsFile
-  launch: "Jenkins test", //Update as per you needs
+  launch: "Automated Test Execution", //Update as per you needs
   description: process.env.RP_DESCRIPTION, //Do not modify - Env variable passed in jenkinsFile
   includeTestSteps: true, //Do not modify
 };
--- tests/BE/Tests/ContentApiTest.spec.js
+++ tests/BE/Tests/ContentApiTest.spec.js
@@ -1,5 +1,5 @@
 const { test, expect, request } = require('@playwright/test');
-import * as testDataForContent from '../../../test-data/test-data-for-content-endpoint';
+import * as testDataForContent from '../../../test-data/test-data-for-content-endpoint.js'
 
 test.describe('Tests for metrics-expose API @ONEPLFR-352', async () => {
     
@@ -15,82 +15,10 @@ test.describe('Tests for metrics-expose API @ONEPLFR-352', async () => {
           
         await expect(response.ok()).toBeTruthy()
         
-        await expect(jsonPath(responseBody, "$.url")).to
-    })
-
-    test('Request without Api Key header', async ({request}) => {
-        const response = await request.get('', {
-            headers: testDataForContent.headerWithoutApiKey,
-            params: testDataForContent.paramsBody
+        const responseFromPresignedUrl = await request.get(responseBody.url, {
+            headers: testDataForContent.headerWithApiKey
         })
-        const responseBody = await response.json()
 
-        // await expect(response.ok()).toBeFalsy()
-        // await expect(responseBody).toEqual(testDataForContent.errorResponseNone)
-    })
-
-    test('Request with wrong Api Key header', async ({request}) => {
-        const response = await request.get('', {
-            headers: testDataForContent.headerWithWrongApiKey,
-            params: testDataForContent.paramsBody
-        })
-        const responseBody = await response.json()
-
-        // await expect(response.ok()).toBeFalsy()
-        // await expect(responseBody).toEqual(testDataForContent.errorResponseWrong)
-    })
-
-    test('Request without parameters', async ({request}) => {
-        const response = await request.get('', {
-            headers: testDataForContent.headerWithWrongApiKey
-        })
-        const responseBody = await response.json()
-
-        // await expect(response.ok()).toBeFalsy()
-        // await expect(responseBody).toEqual()  //Will update after resolving current defect
-    })
-
-    test('Request with only date', async ({request}) => {
-        const response = await request.get('', {
-            headerWithApiKey: testDataForContent.headerWithApiKey,
-            params: testDataForContent.paramsBody['date']
-        })
-        const responseBody = await response.json()
-
-        // await expect(response.ok()).toBeFalsy()
-        // await expect(responseBody).toEqual()  //Will update after resolving current defect
-    })
-
-    test('Request with only leanIxId', async ({request}) => {
-        const response = await request.get('', {
-            headerWithApiKey: testDataForContent.headerWithApiKey,
-            params: testDataForContent.paramsBody['leanIxId']
-        })
-        const responseBody = await response.json()
-        
-        // await expect(response.ok()).toBeFalsy()
-        // await expect(responseBody).toEqual()  //Will update after resolving current defect
-    })
-
-    test('Request with different Content-Type', async ({request}) => {
-        const response = await request.get('', {
-            headerWithDifferentContentType: testDataForContent.headerWithDifferentContentType, 
-            params: testDataForContent.paramsBody
-        })
-        const responseBody = await response.json()
-
-        await expect(response.ok()).toBeFalsy()
-        // await expect(responseBody).toEqual()  //Will update after resolving current defect
-    })
-
-    test('Request with different values', async ({request}) => {
-        const response = await request.get('', {
-            headerWithApiKey: testDataForContent.headerWithApiKey, 
-            params: testDataForContent.paramsBodyDifferent
-        })
-        const responseBody = await response.json()
-
-        // await expect(response.ok()).toBeTruthy()
-        // await expect(responseBody).toEqual()  //Will update after resolving current defect
+        console.log(responseFromPresignedUrl.json())
     })
 })
